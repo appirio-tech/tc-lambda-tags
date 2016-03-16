d834d35df4c06fa58f06a08d9ed2b1977b905194
Parth Shah <parth@topcoder.com>
2016-03-15 23:40:47 -0700

    updating deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,15 @@
 set -e # exit with nonzero exit code if anything fails
 echo "Starting Deployment"
 
-if [ ${TRAVIS_BRANCH} == dev ]; then
-    AWS_ACCESS_KEY=${DEV_AWS_KEY}
-    AWS_ACCESS_KEY_SECRET=${DEV_AWS_SECRET}
-elif [ ${TRAVIS_BRANCH} == release ]; then
-    AWS_ACCESS_KEY=${QA_AWS_KEY}
-    AWS_ACCESS_KEY_SECRET=${QA_AWS_SECRET}
-elif [ ${TRAVIS_BRANCH} == master ]; then
-    AWS_ACCESS_KEY=${PROD_AWS_KEY}
-    AWS_ACCESS_KEY_SECRET=${PROD_AWS_SECRET}
+if [ "$TRAVIS_BRANCH" = "dev" ]; then
+  export AWS_ACCESS_KEY=$DEV_AWS_KEY
+  export AWS_ACCESS_KEY_SECRET=$DEV_AWS_SECRET
+elif [ "$TRAVIS_BRANCH" = "release" ]; then
+  export AWS_ACCESS_KEY=$QA_AWS_KEY
+  export AWS_ACCESS_KEY_SECRET=$QA_AWS_SECRET
+elif [ "$TRAVIS_BRANCH" = "master" ]; then
+  export AWS_ACCESS_KEY=$PROD_AWS_KEY
+  export AWS_ACCESS_KEY_SECRET=$PROD_AWS_SECRET
 fi
-
-export AWS_ACCESS_KEY
-export AWS_ACCESS_KEY_SECRET
 
 ./node_modules/.bin/node-lambda deploy
