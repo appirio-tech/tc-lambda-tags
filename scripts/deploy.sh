e8286311d710117b80404c8f50ccd29757b2b2dc
Parth Shah <parth@topcoder.com>
2016-03-15 23:05:14 -0700

    using node-lambda instead of gulp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
-
 echo "Starting Deployment"
-./node_modules/.bin/gulp deploy
+
+if [ ${TRAVIS_BRANCH} == dev ]; then
+    AWS_ACCESS_KEY=${DEV_AWS_KEY}
+    AWS_ACCESS_KEY_SECRET=${DEV_AWS_SECRET}
+elif [ ${TRAVIS_BRANCH} == release ]; then
+    AWS_ACCESS_KEY=${QA_AWS_KEY}
+    AWS_ACCESS_KEY_SECRET=${QA_AWS_SECRET}
+elif [ ${TRAVIS_BRANCH} == master ]; then
+    AWS_ACCESS_KEY=${PROD_AWS_KEY}
+    AWS_ACCESS_KEY_SECRET=${PROD_AWS_SECRET}
+fi
+
+export AWS_ACCESS_KEY
+export AWS_ACCESS_KEY_SECRET
+
+./node_modules/.bin/node-lambda deploy
