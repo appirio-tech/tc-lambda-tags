7c136f03645f87e0556080f6ea97afb658a6cf5e
Parth Shah <parth@topcoder.com>
2016-03-15 23:45:42 -0700

    updating deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,14 @@ echo "Starting Deployment"
 
 if [ "$TRAVIS_BRANCH" = "dev" ]; then
   export AWS_ACCESS_KEY=$DEV_AWS_KEY
-  export AWS_ACCESS_KEY_SECRET=$DEV_AWS_SECRET
+  export AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET
 elif [ "$TRAVIS_BRANCH" = "release" ]; then
   export AWS_ACCESS_KEY=$QA_AWS_KEY
-  export AWS_ACCESS_KEY_SECRET=$QA_AWS_SECRET
+  export AWS_SECRET_ACCESS_KEY=$QA_AWS_SECRET
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
   export AWS_ACCESS_KEY=$PROD_AWS_KEY
-  export AWS_ACCESS_KEY_SECRET=$PROD_AWS_SECRET
+  export AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRET
 fi
 
+env
 ./node_modules/.bin/node-lambda deploy
