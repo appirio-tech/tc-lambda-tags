b1b79f32307f2c9db998d8820603e37446747365
Parth Shah <parth@topcoder.com>
2016-03-25 17:02:41 -0700

    setting up keys for qa & prod

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@
 if [ "$TRAVIS_BRANCH" = "dev" ]; then
   export AWS_ACCESS_KEY_ID=$DEV_AWS_KEY
   export AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET
+  export TAGS_ES_HOST=$DEV_TAGS_ES_HOST
 elif [ "$TRAVIS_BRANCH" = "release" ]; then
   export AWS_ACCESS_KEY_ID=$QA_AWS_KEY
   export AWS_SECRET_ACCESS_KEY=$QA_AWS_SECRET
+  export TAGS_ES_HOST=$QA_TAGS_ES_HOST
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
   export AWS_ACCESS_KEY_ID=$PROD_AWS_KEY
   export AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRET
+  export TAGS_ES_HOST=$PROD_TAGS_ES_HOST
 fi
 
 ./node_modules/.bin/gulp deploy
